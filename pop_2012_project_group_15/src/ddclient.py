310903616ebd7852978ee573d5d6d44d4da20513
Mattias Kjetselberg <mattias.kjetselberg.3135@student.uu.se>
2012-05-24 01:53:53 +0200

    modified:   pop_2012_project_group_15/src/ddclient.py 	modified:   pop_2012_project_group_15/src/gamerules.pyc 	modified:   pop_2012_project_group_15/src/hello.erl

--- pop_2012_project_group_15/src/ddclient.py
+++ pop_2012_project_group_15/src/ddclient.py
@@ -16,11 +16,10 @@
 #      Move()                                                             #
 #                                                                         #
 ############################################################################
+import os, sys, pygame
 from erlport import Protocol, Port, String
 from threading import Thread
-import os, sys, pygame
 from pygame.locals import *
-
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -58,41 +57,23 @@ class Block(pygame.sprite.Sprite):
 
     def update(self):
         pass
-
-
         
 class ddclient(Protocol):
-    #internal variables
-    _world = {}
-    _objects = {}
-    _width = 0
-    _height = 0
-    _port=0
-    _name=""
+    _port = None
     _hero = DD(400,300)
     allsprites = pygame.sprite.RenderPlain(_hero)
 
     def sendRequest(action):
-        self._port.write(action)
+        if self._port:
+            self._port.write(action)
         
-    #Outgoing functions
-    def connect(self,address):
-        print('Lets do this later when we know how to!')
-        
-    def actionrequest(self,action):
-        print('define the actionrequest function')
-        global player_action
-        player_action=action
-
-    def handle_init(self,port,name):
-        self._port=port
-        self._name=name
-        print str(name) + ' ' +str(self._name)
+    def handle_init(self,port):
+        self._port=port   
 
     def handle_worldinfo(self,worldinfo):
         self.allsprites.empty()
-        self._world=worldinfo
         world=worldinfo.splitlines()
+        
         for i in world:            
             objectInfo = i.split(' ')
             newObject = objectInfo[0]
@@ -105,29 +86,6 @@ class ddclient(Protocol):
             elif newObject == 'Stone':
                 stone = Block(x, y, 'block_circle.bmp')
                 stone.add(self.allsprites)
-
-        return "Hello, %s" % str(newObject) 
-            
-        
-    
-    ##stuff pygame needs to know
-    def mapsizetopygame(self):
-        return str(self._width)+' '+str(self._height)
-    def maptopygame(self):
-        value=""
-        for i in self._world.iterkeys():
-            value +=self._objects[self._world[i]].type+ ' '+str(i[0])+' '+str(i[1])+' '+'\n'
-        return value
-    #high level functions
-
-    def move(self,dx,dy):
-        speed=sqrt(dx*dx+dy*dy)
-        if speed > 2: #speed test
-            dx /= 2/speed
-            dy /= 2/speed
-        self.actionrequest('MOVE '+str(dx) +' '+ str(dy))
-    def fire(self, type):
-        self.actionrequest('FIRE '+str(type))
 
 def main():   
     background = pygame.Surface(screen.get_size())
--- pop_2012_project_group_15/src/hello.erl
+++ pop_2012_project_group_15/src/hello.erl
@@ -17,7 +17,7 @@ ReqData = term_to_binary({hello, 'K_DOWN'}),
       end. 
       
 send2(Port) -> 
-ReqData = term_to_binary({test, 'K_DOWN_0'}),
+ReqData = term_to_binary({worldinfo, 'Player 100 100'}),
 
     % Send binary data to hello.py script
     port_command(Port, ReqData),
@@ -29,8 +29,120 @@ ReqData = term_to_binary({test, 'K_DOWN_0'}),
         5000 ->
             {error, timeout}
       end.
-          
+
 send3(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 105 105'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send4(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 110 110'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send5(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 115 115'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send6(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 120 120'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send7(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 125 125'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send8(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 130 130'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send9(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 135 135'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send10(Port) -> 
+ReqData = term_to_binary({worldinfo, 'Player 140 140'}),
+
+    % Send binary data to hello.py script
+    port_command(Port, ReqData),
+    % Wait for reply from hello.py script
+    receive
+        {Port, {data, RespData}} ->
+        	io:format("hej~s~n", [(binary_to_list(RespData))])
+    after
+        5000 ->
+            {error, timeout}
+      end.
+
+send11(Port) -> 
 ReqData = term_to_binary({worldinfo, 'Stone 500 400'}),
 
     % Send binary data to hello.py script
@@ -65,7 +177,57 @@ hello() ->
             {error, timeout}
       end,
 
-send3(Port).
+
+send2(Port),
+timer:sleep(10),
+send3(Port),
+timer:sleep(10),
+send4(Port),
+timer:sleep(10),
+send5(Port),
+timer:sleep(10),
+send6(Port),
+timer:sleep(10),
+send7(Port),
+timer:sleep(10),
+send8(Port),
+timer:sleep(10),
+send9(Port),
+timer:sleep(10),
+send10(Port),
+timer:sleep(10),
+send9(Port),
+timer:sleep(10),
+send8(Port),
+timer:sleep(10),
+send7(Port),
+timer:sleep(10),
+send6(Port),
+timer:sleep(10),
+send5(Port),
+timer:sleep(10),
+send4(Port),
+timer:sleep(10),
+send3(Port),
+timer:sleep(10),
+send2(Port),
+timer:sleep(10),
+send3(Port),
+timer:sleep(10),
+send4(Port),
+timer:sleep(10),
+send5(Port),
+timer:sleep(10),
+send6(Port),
+timer:sleep(10),
+send7(Port),
+timer:sleep(10),
+send8(Port),
+timer:sleep(10),
+send9(Port),
+timer:sleep(10),
+send10(Port).
+
     
 
   
